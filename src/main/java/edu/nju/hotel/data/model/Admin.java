f168f9a2652cc1e96bbeaf64e36bc22c08390b44
zxf14 <1048043763@qq.com>
2017-02-20 10:54:55 +0800

    Admin model

---
+++ src/main/java/edu/nju/hotel/data/model/Admin.java
@@ -0,0 +1,68 @@
+package edu.nju.hotel.data.model;
+
+import javax.persistence.Basic;
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.Id;
+
+/**
+ * Created by zhouxiaofan on 2017/2/9.
+ */
+@Entity
+public class Admin {
+    private int id;
+    private String name;
+    private String psw;
+
+    @Id
+    @Column(name = "id", nullable = false)
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    @Basic
+    @Column(name = "name", nullable = false, length = 45)
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    @Basic
+    @Column(name = "psw", nullable = false, length = 45)
+    public String getPsw() {
+        return psw;
+    }
+
+    public void setPsw(String psw) {
+        this.psw = psw;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+
+        Admin admin = (Admin) o;
+
+        if (id != admin.id) return false;
+        if (name != null ? !name.equals(admin.name) : admin.name != null) return false;
+        if (psw != null ? !psw.equals(admin.psw) : admin.psw != null) return false;
+
+        return true;
+    }
+
+    @Override
+    public int hashCode() {
+        int result = id;
+        result = 31 * result + (name != null ? name.hashCode() : 0);
+        result = 31 * result + (psw != null ? psw.hashCode() : 0);
+        return result;
+    }
+}
